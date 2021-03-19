5d5d743e7e6776cac37c9a8081ed1383f868fff9
Erik Diniz Costa Lopes Lindby <ercos18@student.sdu.dk>
2021-03-19 12:23:48 +0100

    Imported sdk library for the RPLidar. (And fixed relative includes)

--- includes/lidar.h
+++ includes/lidar.h
@@ -6,6 +6,11 @@
  *					University of Southern Denmark
  * Creation date:   12-02-2021
  */
+#include <iostream>
+#include "defines.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "rplidar.h" //RPLIDAR standard sdk, all-in-one header
 
 class LIDAR
 {
